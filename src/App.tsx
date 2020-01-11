372c9a8915a7342911d51d08c37afd66053efe90
Aleister Coinley <coinyon@uberhax0r.de>
2020-01-11 18:54:35 +0100

    Add semantic-ui as ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import logo from './logo.png';
-import hexagon from './hexagon.svg';
-import './App.css';
 import Web3 from 'web3';
 import { useWeb3React } from '@web3-react/core'
 import { InjectedConnector } from '@web3-react/injected-connector'
@@ -10,6 +7,12 @@ import moment from 'moment';
 import ical from 'ical-generator';
 import { sort } from 'ramda';
 import download from 'downloadjs';
+import { Button, Form, Grid, List, Label, Icon, Header, Image, Table, Card, Segment } from 'semantic-ui-react'
+
+import logo from './logo.png';
+import hexagon from './hexagon.svg';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 
 const injected = new InjectedConnector({ supportedChainIds: [1] })
 const referalAddr = '0xFa2C0AbdaeDc8099887914Ab25AD11B3846655B9'
@@ -37,11 +40,11 @@ const ShortAddr: React.FC<{ address: string }> = ({ address }) => {
 
 const StakeRow: React.FC<{ stake: Stake, currentDay: moment.Moment }> = ({ stake }) => {
   return (
-    <tr key={stake.stakeId}>
-      <td>{stake.stakeId}</td>
-      <td>{stake.unlockDay.calendar()}</td>
-      <td>{stake.unlockDay.fromNow()}</td>
-    </tr>
+    <Table.Row key={stake.stakeId}>
+      <Table.Cell>{stake.stakeId}</Table.Cell>
+      <Table.Cell>{stake.unlockDay.calendar()}</Table.Cell>
+      <Table.Cell>{stake.unlockDay.fromNow()}</Table.Cell>
+    </Table.Row>
   );
 }
 
@@ -137,49 +140,72 @@ Reminder created by HEXCAL - https://coinyon.github.io/hexcal/`
   const currentDay = moment();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>HEXCAL</h2>
-        <h4>Do not miss your unlock days</h4>
-        <p>
-        HEXCAL allows you to add the unlock days of your <a className="App-link" href={"https://go.hex.win/?r=" + referalAddr} target='_blank' rel="noopener noreferrer">HEX</a> stakes to your calendar.
-        <br />Download an iCAL/ICS file and import it into your calendar app.
-        </p>
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle' className="App">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="header">
+          <Image src={logo} size="big" className="App-logo" alt="logo" />
+          <h2>HEXCAL</h2>
+          <h4>Do not miss your unlock days</h4>
+        </Header>
+        <Form size='large'>
         {!active ?
-          <p>
-            <button className="App-button" onClick={() => activate()}>
-              Connect to Web3
-            </button>
-          {error ? "error" : null}
-          </p> : null }
-        {(active && account) ?
           <>
-            <h4>Open HEX Stakes for <ShortAddr address={account} /></h4>
+          <Segment>
+            HEXCAL allows you to add the unlock days of your <a className="App-link" href={"https://go.hex.win/?r=" + referalAddr} target='_blank' rel="noopener noreferrer">HEX</a> stakes to your calendar.
+            <br />
+            Download an iCAL/ICS file and import it into your calendar app.
+          </Segment>
+          <Segment placeholder>
+            <Button primary onClick={() => activate()}>
+              Connect to Web3
+            </Button>
+          {error ? "error" : null}
+          </Segment>
+          </>: null }
+        {(active && account) ?
+          <Card fluid>
+            <Card.Content header="Open HEX stakes" />
+        <Card.Content>
+          <List>
+            <List.Item>
+              <Label>
+                <ShortAddr address={account} />
+                <Icon name='delete' />
+              </Label>
+            </List.Item>
+          </List>
+        </Card.Content>
+        <Card.Content>
+          <Segment loading={loading} basic>
             { loading ?
               'Loading...' :
               <>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Unlock day</th>
-                      <th>Interval</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {sortedStakes(stakes).map((stake: Stake) => <StakeRow key={stake.stakeId} stake={stake} currentDay={currentDay} />)}
-                  </tbody>
-                </table>
-                <p>
-                <button className="App-button" onClick={() => downloadIcal()}>
-                  Download as iCal/ICS
-                </button>
-                </p>
+              <Table basic='very' fluid celled>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>ID</Table.HeaderCell>
+                    <Table.HeaderCell>Unlock day</Table.HeaderCell>
+                    <Table.HeaderCell>Interval</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {sortedStakes(stakes).map((stake: Stake) => <StakeRow key={stake.stakeId} stake={stake} currentDay={currentDay} />)}
+                </Table.Body>
+                </Table>
               </> }
-          </>
+          </Segment>
+          </Card.Content>
+          <Card.Content>
+                <p>
+                <Button primary disabled={loading} onClick={() => downloadIcal()}>
+                  Download as iCal/ICS
+                </Button>
+                </p>
+              </Card.Content>
+              </Card>
+
           : null }
-        <p>
+        <Segment>
         Made with
         {" "}
         <a
@@ -202,9 +228,10 @@ Reminder created by HEXCAL - https://coinyon.github.io/hexcal/`
         |
         {" "}
         <a className="App-link" href={"https://etherscan.io/address/" + referalAddr}>Donate</a>
-        </p>
-      </header>
-    </div>
+        </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
   );
 }
 
